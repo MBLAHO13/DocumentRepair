4949ba0f2bd1c99f2be7af06d2de4ca21671f397
Max M <maxbot100@yahoo.com>
2014-05-20 13:52:15 -0400

    Documentation, Slight changes to parseFiles
    Documentation vastly improved. Switched to """ as that is docstring
    convention. Added *args and mode to parseFiles so that it can open files
    in different modes and pass arguments to parseFunc. Changed a few names
    and added comments to clarify code

--- databaseConstructor.py
+++ databaseConstructor.py
@@ -1,40 +1,49 @@
-# Database Constructor
-# 
-# Max Mattes
-# 05/20/2014
+"""Database constructor
+Builds a database of word counts from input .txt files. The database is built
+using dataConnectionFunctions.py and consists of a series of tables labeled by
+their base word and containing three columns: a list of words, the number of
+times that word occurred immediately following the base word, and the number of
+times it occurred as the word after that. For example, in the sentence "Dogs
+are animals", "are" is immediately following "Dogs" and "animals" is the word
+after that, referred to internally as "twice following"
+
+Max Mattes, Karishma Changlani, Colleen Blaho
+05/20/2014
+
+Written for Python 2.7
+"""
 
 import sys
 import dataConnectionFunctions as db
 
-def parseUserFile(parseFunc,fileType,inputMessage) :
-	'''
-	Requests user input for filename and attepts to open file and parse based
-	on given parsing function. 'parseFunc' is the parsing function which
-	accepts a file object as input and 'fileType' is the chosen filetype to be 
-	parsed as a string of the extension. Example: '.txt'
-	inputMessage is the message to be shown to the user requesting a filename.
-	Function will continue processing documents until the user exits.
-	'''
+def parseFiles(fileExt,mode,inputMessage,parseFunc,*args) :
+	"""Continually requests user input files and processes with parseFunc
+	Function Arguments
+	fileExt: Extension for file to be parsed
+	mode: Python-defined modes for opening a file (r,w,rw,...)
+	inputMessage: Message shown to user requesting file names for processing
+	parseFunc: Function to be run on opened files. Must accept file object as
+	input.
+	*args: Arguments to be pased to parseFunc after the file object.
+	"""
 	userInput = ''
 	while userInput != 'EXIT' :
 		userInput = raw_input(inputMessage)
-		if userInput.endswith(fileType) :
+		if userInput.endswith(fileExt) :
 			try :
-				userFile = open(userInput, 'r')
+				 with open(userInput,mode) as userFile :
+					parseFunc(userFile,*args)
 			except IOError :
-				print 'Failed to open file. Check to make sure it is in the ' + \
-					'current working directory'
-			else :
-				parseFunc(userFile)
+				print 'Failed to open file correctly. Check to make sure it' +\
+					' is in the current working directory'
 		elif userInput != 'EXIT' :
 			print 'Incorrect file extension'
 
 def fileToDatabase(inputFile) :
-	'''
-	Takes a file object and inserts word triplets into the database after
-	parsing and cleaning the input. Throws IOError if file not opened for
-	reading.
-	'''
+	"""Builds database of word triplets from file object
+	Function Arguments
+	inputFile: File object to read from
+	"""
 	current = fol = twiceFol = '.'
 	# Splitting into words
 	for word in inputFile.read().replace('--',' ').split() :
@@ -43,33 +52,34 @@ def fileToDatabase(inputFile) :
 		# Ignoring blanks
 		if word == '' :
 			continue
+			
 		# Establishing punctuation as separate 'word' in database
-		if word[len(word)-1] in ['.','?','!'] :
+		if word[-1] in ['.','?','!'] :
 			word = word.strip('.?!')
-			current,fol,twiceFol,word = fol,twiceFol,word,'.'
+			current,fol,twiceFol,word = fol,twiceFol,word,'.' # Shifting buffer
 			addToDB(current,fol,twiceFol)
+			
 		# Moving buffer and adding to database
 		current,fol,twiceFol = fol,twiceFol,word
 		if not (current == '.' and fol == '.') :
 			addToDB(current,fol,twiceFol)
 		
 def addToDB(base,fol,twiceFol) :
-	'''
-	Adds word triplet to database using dataConnectionFunctions
-	'''
+	"""Adds word triplet to database using dataConnectionFunctions
+	Function Arguments
+	base: Base word to be used as table name
+	fol: Following word to be added to table
+	twiceFol: Twice following word to be added to table
+	"""
 	print base, ':', fol, ':', twiceFol
 	# db.insert(base,fol,1)
 	# db.insert(base,twiceFol,0)
 				
 def main() :
-	'''
-	Accepts user input files using console and parses the words into the
-	database defined in dataConnectionFunctions. Words are collected and
-	counted to produce probabilities to be used later by the document
-	repair module.
-	'''
-	parseUserFile(fileToDatabase,'.txt','Please enter a .txt document to ' + \
-			' be parsed and added to the database or "EXIT"\nto quit\n>>> ')
+	"""Runs fileToDatabase in parseFiles to create a database from .txt files
+	"""
+	parseFiles('.txt','r','Please enter a .txt document to be parsed and ' + \
+		'added to the database or "EXIT"\nto quit\n>>> ',fileToDatabase,5)
 	return 0
 
 if __name__ == '__main__' :
